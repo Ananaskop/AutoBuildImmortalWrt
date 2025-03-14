b55adaf614804197772b06268fff18fbcaa5283a
HgdyskjruyiGHDsd <ananaskop@icloud.com>
2025-03-14 21:03:04 +0800

    更新 build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -20,8 +20,6 @@ cat /home/build/immortalwrt/files/etc/config/pppoe-settings
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
-curl -s -L https://github.com/nikkinikki-org/OpenWrt-nikki/raw/refs/heads/main/feed.sh | sh
-
 
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
